e28c77d99e9564e1e9ed13f0255dec8a69de5819
Nick Fox-Gieg <nick@fox-gieg.com>
2022-03-08 15:10:41 -0500

    testing

---
+++ week_9/card_example_js/js/card.js
@@ -0,0 +1,49 @@
+"use strict";
+
+class Card {
+
+	constructor(_front, _back, x, y, w, h) {
+		this.flipped = false;
+		this.clicked = false;
+		this.hovered = false;
+		this.front = _front;
+		this.back = _back;
+		this.pos = createVector(x, y);
+		this.dim = createVector(w, h);
+		this.hdim = createVector(w/2, h/2);
+		this.offset = createVector(0, 0);
+	}
+
+	update() {
+		//
+	}
+
+	draw() {
+		noTint();
+
+		if (this.clicked) {
+			tint(255, 0, 0);
+		}
+
+		imageMode(CENTER);
+	    if (this.flipped) {
+	      image(this.front, this.pos.x, this.pos.y);
+	    } else {
+	      image(this.back, this.pos.x, this.pos.y);
+	    }
+	}
+
+	run() {
+		this.update();
+		this.draw();
+	}
+
+	checkHover() {
+		this.hovered = mouseX > this.pos.x - this.hdim.x && mouseX < this.pos.x + this.hdim.x && mouseY > this.pos.y - this.hdim.y && mouseY < this.pos.y + this.hdim.y;
+	}
+
+	checkClick() {
+		this.clicked = this.hovered && mouseIsPressed;
+	}
+
+}
