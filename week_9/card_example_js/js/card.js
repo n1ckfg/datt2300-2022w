8177fecfd0be8dd1b882614c10754fe30a3dc092
Nick Fox-Gieg <nick@fox-gieg.com>
2022-03-08 15:23:29 -0500

    cards

--- week_9/card_example_js/js/card.js
+++ week_9/card_example_js/js/card.js
@@ -12,16 +12,27 @@ class Card {
 		this.dim = createVector(w, h);
 		this.hdim = createVector(w/2, h/2);
 		this.offset = createVector(0, 0);
+		this.marktime = 0;
 	}
 
 	update() {
-		//
+		if (this.hovered && millis() > this.marktime + 10) {
+			this.hovered = false;
+			this.clicked = false;
+		}
+
+		if (this.clicked) {
+			this.pos.x = mouseX;
+			this.pos.y = mouseY;
+		}
 	}
 
 	draw() {
 		noTint();
 
-		if (this.clicked) {
+		if (this.hovered && !this.clicked) {
+			tint(200, 127, 0);
+		} else if (this.clicked) {
 			tint(255, 0, 0);
 		}
 
@@ -38,12 +49,10 @@ class Card {
 		this.draw();
 	}
 
-	checkHover() {
+	check() {
 		this.hovered = mouseX > this.pos.x - this.hdim.x && mouseX < this.pos.x + this.hdim.x && mouseY > this.pos.y - this.hdim.y && mouseY < this.pos.y + this.hdim.y;
-	}
-
-	checkClick() {
 		this.clicked = this.hovered && mouseIsPressed;
+		this.marktime = millis();
 	}
 
 }
