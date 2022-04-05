ff41d7c6b941fa95c8122d9002cdae5aa9036eb8
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-05 14:23:03 -0400

    bug fixes

--- week_13/Review1/Assets/Scripts/ParallaxControl.cs
+++ week_13/Review1/Assets/Scripts/ParallaxControl.cs
@@ -9,13 +9,15 @@ public class ParallaxControl : MonoBehaviour {
     public Parallaxer[] parallaxers;
 
     private void Update() {
-        for (int i = 0; i < parallaxers.Length; i++) {
-            if (player.isRunning) {
-                parallaxers[i].speed = player.transform.position.x * (i + 1) * parallaxSpeed;
-            } else {
-                parallaxers[i].speed = 0f;
+        if (player.alive) {
+            for (int i = 0; i < parallaxers.Length; i++) {
+                if (player.isRunning) {
+                    parallaxers[i].speed = player.transform.position.x * (i + 1) * parallaxSpeed;
+                } else {
+                    parallaxers[i].speed = 0f;
+                }
+                if (player.screenCoords.x < player.transform.position.x) parallaxers[i].speed = -Mathf.Abs(parallaxers[i].speed);
             }
-            if (player.screenCoords.x < player.transform.position.x) parallaxers[i].speed = -Mathf.Abs(parallaxers[i].speed);
         }
     }
 
--- week_13/Review1/Assets/Scripts/Spawner.cs
+++ week_13/Review1/Assets/Scripts/Spawner.cs
@@ -8,8 +8,11 @@ public class Spawner : MonoBehaviour {
     public float spawnInterval = 1f;
     public float yRange = 3f;
 
+    private Player player;
 
     void Start() {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Player>();
+
         StartCoroutine(makeBomb());
     }
 
@@ -18,7 +21,7 @@ public class Spawner : MonoBehaviour {
     }
 
     IEnumerator makeBomb() {
-        while (true) {
+        while (player.alive) {
             Vector3 pos = new Vector3(transform.position.x, Random.Range(-yRange, yRange), transform.position.z);
             Bomb bomb = GameObject.Instantiate(bombPrefab, pos, transform.rotation);
             bomb.transform.parent = transform;
